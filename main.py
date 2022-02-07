3f73f57940eae123fca320115d336e8c2401fcf1
Robbie McGregor <mr.robbie.mcgregor@gmail.com>
2022-02-07 22:44:00 +1300

    refactor - make functions for apiCall and getYear

--- main.py
+++ main.py
@@ -5,22 +5,30 @@ import datetime
 
 app = Flask(__name__)
 
+# Main Route
 @app.route('/')
 def main():
     api_url = 'https://newsapi.org/v2/top-headlines?country=nz&apiKey=16cd803654d64cbf89e2ec14545f30ad&language=en'
-    api_response = requests.get(api_url)
-    news_items = api_response.json()
-    year = datetime.datetime.now().date().strftime("%Y")
+    news_items = apiCall(api_url)
+    year = getYear()
     return render_template('index.html', articles=news_items['articles'], year=year)
 
+# Route to display articles for specific category
 @app.route('/articles/<category>')
 def articles(category):
     api_url = f"https://newsapi.org/v2/top-headlines?category={category}&apiKey=16cd803654d64cbf89e2ec14545f30ad&language=en&country=nz"
-    api_response = requests.get(api_url)
-    news_items = api_response.json()
-    year = datetime.datetime.now().date().strftime("%Y")
+    news_items = apiCall(api_url)
+    year = getYear()
     return render_template('index.html', articles=news_items['articles'], year=year)
 
+# Make API call and return as JSON
+def apiCall(url):
+    response = requests.get(url)
+    return response.json()
+
+# Get Current Year
+def getYear():
+    return datetime.datetime.now().date().strftime("%Y")
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
